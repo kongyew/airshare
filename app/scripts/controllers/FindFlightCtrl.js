f0d6b3e12de8a2b2d243f200f1c40a791351aab6
King Chung Huang <kinghuang@mac.com>
2014-02-16 15:14:18 -0800

    Changed to use angularjs to load in live data from Neo4j.

--- app/scripts/controllers/FindFlightCtrl.js
+++ app/scripts/controllers/FindFlightCtrl.js
@@ -1,103 +1,104 @@
 'use strict';
 
 angular.module('seatsplitterApp')
-  .controller('FindFlightCtrl', function ($scope) {
+  .controller('FindFlightCtrl', function ($scope,$http) {
 
 // http://mgcrea.github.io/angular-strap/##selects
-    $scope.segments = [
-            {
-               "row":[
-                  "6814",
-                  4,
-                  "silver",
-                  "2014/03/16",
-                  "United Airlines",
-                  "05:10:10",
-                  "San Francisco",
-                  "Calgary",
-                  "United Airlines",
-                  "SFO",
-                  "YYC"
-               ],
-               "graph":{
-                  "nodes":[
+    // $scope.segments = [
+    //         {
+    //            "row":[
+    //               "6814",
+    //               4,
+    //               "silver",
+    //               "2014/03/16",
+    //               "United Airlines",
+    //               "05:10:10",
+    //               "San Francisco",
+    //               "Calgary",
+    //               "United Airlines",
+    //               "SFO",
+    //               "YYC"
+    //            ],
+    //            "graph":{
+    //               "nodes":[
 
-                  ],
-                  "relationships":[
+    //               ],
+    //               "relationships":[
 
-                  ]
-               }
-            },
-            {
-               "row":[
-                  "6814",
-                  4.3,
-                  "silver",
-                  "2014/03/16",
-                  "United Airlines",
-                  "05:10:10",
-                  "San Francisco",
-                  "Calgary",
-                  "Air Canada",
-                  "SFO",
-                  "YYC"
-               ],
-               "graph":{
-                  "nodes":[
+    //               ]
+    //            }
+    //         },
+    //         {
+    //            "row":[
+    //               "6814",
+    //               4.3,
+    //               "silver",
+    //               "2014/03/16",
+    //               "United Airlines",
+    //               "05:10:10",
+    //               "San Francisco",
+    //               "Calgary",
+    //               "Air Canada",
+    //               "SFO",
+    //               "YYC"
+    //            ],
+    //            "graph":{
+    //               "nodes":[
 
-                  ],
-                  "relationships":[
+    //               ],
+    //               "relationships":[
 
-                  ]
-               }
-            },
-            {
-               "row":[
-                  "952",
-                  4,
-                  "gold",
-                  "2014/03/14",
-                  "United Airlines",
-                  "20:10:10",
-                  "San Francisco",
-                  "Nashville",
-                  "United Airlines",
-                  "SFO",
-                  "BNA"
-               ],
-               "graph":{
-                  "nodes":[
+    //               ]
+    //            }
+    //         },
+    //         {
+    //            "row":[
+    //               "952",
+    //               4,
+    //               "gold",
+    //               "2014/03/14",
+    //               "United Airlines",
+    //               "20:10:10",
+    //               "San Francisco",
+    //               "Nashville",
+    //               "United Airlines",
+    //               "SFO",
+    //               "BNA"
+    //            ],
+    //            "graph":{
+    //               "nodes":[
 
-                  ],
-                  "relationships":[
+    //               ],
+    //               "relationships":[
 
-                  ]
-               }
-            },
-            {
-               "row":[
-                  "952",
-                  5,
-                  "gold",
-                  "2014/03/14",
-                  "United Airlines",
-                  "20:10:10",
-                  "San Francisco",
-                  "Nashville",
-                  "Air Canada",
-                  "SFO",
-                  "BNA"
-               ],
-               "graph":{
-                  "nodes":[
+    //               ]
+    //            }
+    //         },
+    //         {
+    //            "row":[
+    //               "952",
+    //               5,
+    //               "gold",
+    //               "2014/03/14",
+    //               "United Airlines",
+    //               "20:10:10",
+    //               "San Francisco",
+    //               "Nashville",
+    //               "Air Canada",
+    //               "SFO",
+    //               "BNA"
+    //            ],
+    //            "graph":{
+    //               "nodes":[
 
-                  ],
-                  "relationships":[
+    //               ],
+    //               "relationships":[
 
-                  ]
-               }
-            }
-         ];
+    //               ]
+    //            }
+    //         }
+    //      ];
+    $scope.segments = [];
     $scope.selectedIcon = "";
    // $scope.selectedIcons = "["Globe","Heart"]";
 
@@ -127,5 +128,34 @@ angular.module('seatsplitterApp')
             },
             zoom: 8
         };
+
+    $http({
+      method: 'POST',
+      url: 'http://localhost:7474/db/data/transaction',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      data: {
+          "statements": [
+              {
+                  "statement" : "MATCH (b:Person)-[:BOOKED_ON]->(c:Flight)<-[:BOOKED_ON]-(a:Person) WHERE b.address = \"brandon.elizabeth@gmail.com\" RETURN c.marketing_flight_number, a.reputation, a.status, c.date, c.marketing_airline, c.time, c.start_city_name, c.end_airport_city, a.lounge, c.start_airport_code, c.end_airport_code ORDER BY c.marketing_flight_number",
+                  "resultDataContents": ["row", "graph"],
+                  "parameters" : {
+                      "props" : {
+                          "name" : "My Node"
+                      }
+                  }
+              }
+          ]
+      },
+      transformRequest: function(data) {
+        return JSON.stringify(data);
+      },
+      transformResponse: function(data) {
+        return JSON.parse(data);
+      }
+    }).success(function(data, status, headers, config) {
+      $scope.segments = data['results'][0]['data'];
+    });
  //   $scope.icons = "[{"value":"Gear","label":"<i class=\"fa fa-gear\"></i> Gear"},{"value":"Globe","label":"<i class=\"fa fa-globe\"></i> Globe"},{"value":"Heart","label":"<i class=\"fa fa-heart\"></i> Heart"},{"value":"Camera","label":"<i class=\"fa fa-camera\"></i> Camera"}]";
   });
