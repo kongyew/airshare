9d61d8ec763aab0fb75a5002e1ead48e6d25c369
King Chung Huang <kinghuang@mac.com>
2014-02-16 14:48:11 -0800

    Updated find flights page.

--- app/scripts/controllers/FindFlightCtrl.js
+++ app/scripts/controllers/FindFlightCtrl.js
@@ -4,9 +4,121 @@ angular.module('seatsplitterApp')
   .controller('FindFlightCtrl', function ($scope) {
 
 // http://mgcrea.github.io/angular-strap/##selects
+    $scope.segments = [
+            {
+               "row":[
+                  "6814",
+                  4,
+                  "silver",
+                  "2014/03/16",
+                  "United Airlines",
+                  "05:10:10",
+                  "San Francisco",
+                  "Calgary",
+                  "United Airlines",
+                  "SFO",
+                  "YYC"
+               ],
+               "graph":{
+                  "nodes":[
+
+                  ],
+                  "relationships":[
+
+                  ]
+               }
+            },
+            {
+               "row":[
+                  "6814",
+                  4.3,
+                  "silver",
+                  "2014/03/16",
+                  "United Airlines",
+                  "05:10:10",
+                  "San Francisco",
+                  "Calgary",
+                  "Air Canada",
+                  "SFO",
+                  "YYC"
+               ],
+               "graph":{
+                  "nodes":[
+
+                  ],
+                  "relationships":[
+
+                  ]
+               }
+            },
+            {
+               "row":[
+                  "952",
+                  4,
+                  "gold",
+                  "2014/03/14",
+                  "United Airlines",
+                  "20:10:10",
+                  "San Francisco",
+                  "Nashville",
+                  "United Airlines",
+                  "SFO",
+                  "BNA"
+               ],
+               "graph":{
+                  "nodes":[
+
+                  ],
+                  "relationships":[
+
+                  ]
+               }
+            },
+            {
+               "row":[
+                  "952",
+                  5,
+                  "gold",
+                  "2014/03/14",
+                  "United Airlines",
+                  "20:10:10",
+                  "San Francisco",
+                  "Nashville",
+                  "Air Canada",
+                  "SFO",
+                  "BNA"
+               ],
+               "graph":{
+                  "nodes":[
+
+                  ],
+                  "relationships":[
+
+                  ]
+               }
+            }
+         ];
     $scope.selectedIcon = "";
    // $scope.selectedIcons = "["Globe","Heart"]";
 
+
+   $scope.reputation = function(val) {
+      var result;
+
+      if (val >= 4.5) {
+        result = "best";
+      }
+
+      if (val >= 3.0 && val < 4.5) {
+        result = "good";
+      }
+
+      if (val < 3.0) {
+        result = "bad";
+      }
+      
+      return result;
+   };
 
     $scope.map = {
             center: {
